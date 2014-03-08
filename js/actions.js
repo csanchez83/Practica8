3bfca151a7b265bdfde9f2505df41773da8b7116
csanchez83 <csanchez83@hotmail.com>
2014-03-08 17:31:29 -0600

    080314

---
+++ js/actions.js
@@ -0,0 +1,90 @@
+//actions
+var jQT = new $.jQTouch({
+	themeSelectionSelector: '#jqt'
+});
+
+function escribir(texto)
+{
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("practica8.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.createWriter(gotFileWriter, fail);
+    }
+
+    function gotFileWriter(writer) {
+        writer.onwriteend = function(evt) 
+        {
+            navigator.notification.alert("El archivo fue escrito satisfactoriamente", null, "Escribir", "Aceptar");    
+        
+        };
+        writer.write(texto);
+    }
+
+    function fail(error) {
+        alert(error.code);
+    }
+}
+
+function leer()
+{
+    
+    document.addEventListener("deviceready", onDeviceReady, false);
+    
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("practica8.txt", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(readAsText, fail);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function(evt) 
+        {
+            //Asigna un valor con val(param), si usamos val() devuelve el valor del textarea
+            $('#arcLeer').val(evt.target.result);
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(evt) {
+        console.log(evt.target.error.code);
+    }
+    
+}
+
+$(function()
+  {
+     $('#archivos .individual li').tap(function()
+                                       {
+                                           if($(this).index() == 0)
+                                           {
+                                               //Se presiono Escribir
+                                               escribir($('#arcEscribir').val());
+                                           }
+                                           else
+                                           {
+                                               //Se presiono Leer
+                                               leer();
+                                           }
+                                               
+                                           
+                                       });
+  });
