7144869c927ba6e22aaa1e99dda1f4e2a7432251
csanchez83 <csanchez83@hotmail.com>
2014-03-08 19:26:17 -0600

    080314

--- js/contactos.js
+++ js/contactos.js
@@ -4,7 +4,7 @@ function listar()
     {
         for (i=0; i <= contacts.length; i++)
         {
-            $('#contactos .plastic').html('<li><a href="tel:' + contacts[i].phoneNumbers[0] + '">' + contacts[i].displayName + '</a></li>');
+            $('#contactos .plastic').append('<li><a href="tel:' + contacts[i].phoneNumbers[0] + '">' + contacts[i].displayName + '</a></li>');
         }
         
         
