244ebbfc81c56d6e884db79cb373bf365fc42529
csanchez83 <csanchez83@hotmail.com>
2014-03-08 19:19:23 -0600

    080314

--- js/contactos.js
+++ js/contactos.js
@@ -19,8 +19,8 @@ function listar()
 
     // find all contacts with 'Bob' in any name field
     var options      = new ContactFindOptions();
-    options.filter   = "c";
-    options.multiple = true;
+    //options.filter   = "c";
+    //options.multiple = true;
     var fields       = ["*"];
     navigator.contacts.find(fields, onSuccess, onError, options);
     
